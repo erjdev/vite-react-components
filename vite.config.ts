959f8dfd5e65892f537ca1674d63cb205e9f3630
Erik <erik@boxhammer.dev>
2024-06-29 20:25:24 -0400

    Updated build to be more treeshakeable, added CSS generation

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path, { resolve } from 'path'
 import dts from 'vite-plugin-dts';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     dts({
-    include: ['lib'],
+      include: ['lib'],
       tsconfigPath: 'tsconfig.lib.json',
       rollupTypes: true,
     }),
+    libInjectCss(),
   ],
   server: {
     port: 3000,
@@ -20,7 +22,12 @@ export default defineConfig({
   build: {
     copyPublicDir: false,
     lib: {
-      entry: resolve(__dirname, 'lib/main.ts'),
+      entry: {
+        index: resolve(__dirname, 'lib/index.ts'),
+        button: resolve(__dirname, 'lib/components/Button/index.tsx'),
+        input: resolve(__dirname, 'lib/components/Input/index.tsx'),
+        label: resolve(__dirname, 'lib/components/Label/index.tsx'),
+      },
       formats: ['es'],
       name: 'ViteReactComponents',
       fileName: (format) => `vrc.${format}.js`,
@@ -28,6 +35,12 @@ export default defineConfig({
     rollupOptions: {
       external: ['react', 'react/jsx-runtime', 'react-dom'],
       output: {
+        // Put chunk files at <output>/chunks
+        chunkFileNames: 'chunks/[name].[hash].js',
+        // Put chunk styles at <output>/assets
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js',
+        // Register externals/globals
         globals: {
           react: "React",
           "react-dom": "ReactDOM",
@@ -42,6 +55,6 @@ export default defineConfig({
     },
   },
   test: {
-    include: ['./vitest/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']
+    include: ['./vitest/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   },
 });
