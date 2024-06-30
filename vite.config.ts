afe37102eefdefda335da26610617e0f4e441b7e
Erik <erik@boxhammer.dev>
2024-06-29 22:59:04 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -52,7 +52,8 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './lib/index'),
+      '@/*': path.resolve(__dirname, './lib/*'),
     },
   },
   test: {
